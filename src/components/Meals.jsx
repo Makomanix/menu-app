8e48ba00566639b59e3f169e949e27714de0d66c
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2024-07-05 16:56:31 -0400

    created useHttp.js to handle fetch

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,27 +1,20 @@
-import { useEffect, useState } from "react";
-
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
-export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
-  
-  useEffect(() => {
-    async function fetchMeals() { 
-      const response = await fetch('http://localhost:3000/meals');
-  
-      if (!response.ok) {
-          //...
-      }
-  
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
+const requestConfig = {};
 
-    fetchMeals();
-  }, []);
+export default function Meals() {
+  const {
+    data: loadedMeals,
+    isLoading,
+    error
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+  console.log(loadedMeals)
+
 
   return (
-    <ul id="meals">
+    <ul id="meals"> 
       {loadedMeals.map((meal) => (
         <MealItem key={meal.id} meal={meal} />
       ))}
