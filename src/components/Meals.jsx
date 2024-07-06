75986bb3ac71123979f0d1d758eecbe5a0f8ae93
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2024-07-06 14:08:06 -0400

    configured useHttp to process checkout. Handled errors and loading

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
+import Error from "./Error";
 
 const requestConfig = {};
 
@@ -10,8 +11,13 @@ export default function Meals() {
     error
   } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-  console.log(loadedMeals)
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>
+  }
 
+  if (error) {
+    return <Error title='Failed to fetch meals' message={error}/>
+  }
 
   return (
     <ul id="meals"> 
